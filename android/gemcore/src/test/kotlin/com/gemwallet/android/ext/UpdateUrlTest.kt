0b0f5db84c4e076ab1dedd7d242096e1ea61bd9b
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-04-12 03:08:34 +0000

    Improve universal flavor url

---
+++ android/gemcore/src/test/kotlin/com/gemwallet/android/ext/UpdateUrlTest.kt
@@ -0,0 +1,31 @@
+package com.gemwallet.android.ext
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UpdateUrlTest {
+
+    @Test
+    fun `universal flavor uses versioned apk url`() {
+        assertEquals(
+            "https://apk.gemwallet.com/gem_wallet_universal_v2.29.apk",
+            updateUrl(
+                flavor = "universal",
+                version = "2.29",
+                fallbackUrl = "https://apk.gemwallet.com/gem_wallet_latest.apk",
+            )
+        )
+    }
+
+    @Test
+    fun `non universal flavors keep fallback url`() {
+        assertEquals(
+            "https://play.google.com/store/apps/details?id=com.gemwallet.android",
+            updateUrl(
+                flavor = "google",
+                version = "2.29",
+                fallbackUrl = "https://play.google.com/store/apps/details?id=com.gemwallet.android",
+            )
+        )
+    }
+}
