46afec4e6e2d390d9e6f2c3d803851aa944ab3d5
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-05-19 02:08:26 +0300

    Promote formatPnlWithPercentage to gemcore for shared use

--- android/gemcore/src/main/kotlin/com/gemwallet/android/domains/perpetual/PerpetualPnlFormatter.kt
+++ android/gemcore/src/main/kotlin/com/gemwallet/android/domains/perpetual/PerpetualPnlFormatter.kt
@@ -1,14 +1,14 @@
-package com.gemwallet.android.data.coordinators.perpetuals
+package com.gemwallet.android.domains.perpetual
 
 import com.gemwallet.android.domains.percentage.formatAsPercentage
 import com.gemwallet.android.model.formatPnl
 import com.wallet.core.primitives.Currency
 
-internal fun formatPnlWithPercentage(pnl: Double, marginAmount: Double): String {
+fun formatPnlWithPercentage(pnl: Double, marginAmount: Double, dynamicPlace: Boolean = false): String {
     val percentage = if (marginAmount > 0.0) {
         (pnl / marginAmount) * 100.0
     } else {
         0.0
     }.formatAsPercentage()
-    return "${Currency.USD.formatPnl(pnl)} ($percentage)"
+    return "${Currency.USD.formatPnl(pnl, dynamicPlace = dynamicPlace)} ($percentage)"
 }
