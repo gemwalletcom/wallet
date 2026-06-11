ad298181caee04e0fbf1ee1f5e78b7f015dcccac
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-06-11 22:58:48 +0300

    Use perpetual fixture in search test and drop migration test

--- android/data/repositories/src/test/kotlin/com/gemwallet/android/data/repositories/tokens/WalletSearchTokensTest.kt
+++ android/data/repositories/src/test/kotlin/com/gemwallet/android/data/repositories/tokens/WalletSearchTokensTest.kt
@@ -5,8 +5,8 @@ import com.gemwallet.android.data.repositories.perpetual.PerpetualRepository
 import com.gemwallet.android.data.service.store.database.SearchPriorityDao
 import com.gemwallet.android.testkit.mockAsset
 import com.gemwallet.android.testkit.mockAssetBasic
+import com.gemwallet.android.testkit.mockPerpetual
 import com.wallet.core.primitives.Currency
-import com.wallet.core.primitives.Perpetual
 import com.wallet.core.primitives.PerpetualId
 import com.wallet.core.primitives.PerpetualProvider
 import com.wallet.core.primitives.PerpetualSearchData
@@ -36,19 +36,12 @@ class WalletSearchTokensTest {
     @Test
     fun search_ingestsPerpetualsAndStoresPerpPriority() = runTest {
         val perpAsset = mockAsset()
-        val perpetual = Perpetual(
+        val perpetual = mockPerpetual(
             id = PerpetualId(provider = PerpetualProvider.Hypercore, symbol = "BTC"),
             name = "Bitcoin",
-            provider = PerpetualProvider.Hypercore,
             assetId = perpAsset.id,
-            identifier = "0",
             price = 1.0,
-            pricePercentChange24h = 0.0,
-            openInterest = 0.0,
             volume24h = 1.0,
-            funding = 0.0,
-            maxLeverage = 1u,
-            isIsolatedOnly = false,
         )
         coEvery {
             gemSearch.search(query = "btc", chains = emptyList(), tags = emptyList())
