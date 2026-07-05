5de0dc4225c875b493ba6a67c51d5f05902ed5f3
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-07-05 03:58:10 +0000

    Bump to 2.98 (iOS 155, Android 797)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.gemwallet.android"
         minSdk = 28
         targetSdk = 37
-        versionCode = 796
-        versionName = "2.97"
+        versionCode = 797
+        versionName = "2.98"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
