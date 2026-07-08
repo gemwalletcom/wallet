35d1c1a1e2b2d97b940c697b14e6fc30d34a1f75
Radmir <radmir@gemwallet.com>
2026-07-08 18:22:24 +0400

    fix(android): align section headers and swap inputs with list content

--- android/ui/src/main/kotlin/com/gemwallet/android/ui/components/list_item/SectionHeaderItem.kt
+++ android/ui/src/main/kotlin/com/gemwallet/android/ui/components/list_item/SectionHeaderItem.kt
@@ -5,9 +5,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import com.gemwallet.android.ui.models.ListPosition
+import com.gemwallet.android.ui.theme.adaptivePadding
 import com.gemwallet.android.ui.theme.paddingDefault
+import com.gemwallet.android.ui.theme.paddingMiddle
+import com.gemwallet.android.ui.theme.paddingSmall
 
-val sectionHeaderHorizontalPadding = paddingDefault * 2
+val sectionHeaderHorizontalPadding: Dp
+    @Composable get() = adaptivePadding(default = paddingDefault, compact = paddingSmall) + paddingMiddle
 
 @Composable
 fun Modifier.sectionHeaderItem(paddingVertical: Dp? = null): Modifier = fillMaxWidth().listItem(
