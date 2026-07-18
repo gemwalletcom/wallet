2ea61118045e35e9615a922f03342fcaf7b5eec0
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-07-18 15:02:49 +0000

    Fix Ankr token transfer transaction indexing

--- core/crates/gem_evm/src/rpc/ankr/model.rs
+++ core/crates/gem_evm/src/rpc/ankr/model.rs
@@ -1,17 +1,33 @@
 use num_bigint::BigUint;
 use serde::Deserialize;
-use serde_serializers::deserialize_biguint_from_str;
+use serde_serializers::{deserialize_biguint_from_str, deserialize_u64_from_str_or_int};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub(super) struct Transaction {
     pub(super) hash: String,
+    #[serde(deserialize_with = "deserialize_u64_from_str_or_int")]
+    pub(super) timestamp: u64,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub(super) struct Transactions {
     pub(super) transactions: Vec<Transaction>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(super) struct TokenTransfer {
+    pub(super) transaction_hash: String,
+    #[serde(deserialize_with = "deserialize_u64_from_str_or_int")]
+    pub(super) timestamp: u64,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(super) struct TokenTransfers {
+    pub(super) transfers: Vec<TokenTransfer>,
 }
 
 #[derive(Debug, Deserialize)]
