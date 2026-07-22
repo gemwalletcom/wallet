bedfd300e0f54537e279f07ae20c363629f9f522
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-07-22 19:52:27 +0300

    Remove unused files on Android

--- android/app/src/main/kotlin/com/gemwallet/android/features/onboarding/OnboardEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.gemwallet.android.features.onboarding
-
-sealed interface OnboardEvent {
-    data object ImportClick : OnboardEvent
-
-    data object CreateClick : OnboardEvent
-}
--- android/blockchain/src/main/kotlin/com/gemwallet/android/blockchain/clients/BlockchainClient.kt
+++
@@ -1,9 +0,0 @@
-package com.gemwallet.android.blockchain.clients
-
-import com.wallet.core.primitives.Chain
-
-interface BlockchainClient {
-    fun supported(chain: Chain): Boolean
-}
-
-fun <T: BlockchainClient> List<T>.getClient(chain: Chain) = firstOrNull { it.supported(chain) }
