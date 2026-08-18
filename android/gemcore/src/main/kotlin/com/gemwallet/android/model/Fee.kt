43be2ce2d02b9c95b7e1226845504ee6b8d47ff4
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-08-18 20:31:16 +0000

    Centralize shared fee properties

--- android/gemcore/src/main/kotlin/com/gemwallet/android/model/Fee.kt
+++ android/gemcore/src/main/kotlin/com/gemwallet/android/model/Fee.kt
@@ -4,48 +4,47 @@ import com.wallet.core.primitives.AssetId
 import com.wallet.core.primitives.FeePriority
 import java.math.BigInteger
 
-sealed interface Fee {
-
-    val priority: FeePriority
-    val feeAssetId: AssetId
-    val options: Map<String, BigInteger>
-
-    val amount: BigInteger
+sealed class Fee(
+    val feeAssetId: AssetId,
+    val priority: FeePriority,
+    val amount: BigInteger,
+    val options: Map<String, BigInteger>,
+) {
 
     class Plain(
-        override val feeAssetId: AssetId,
-        override val priority: FeePriority,
-        override val amount: BigInteger,
-        override val options: Map<String, BigInteger>,
-    ) : Fee
+        feeAssetId: AssetId,
+        priority: FeePriority,
+        amount: BigInteger,
+        options: Map<String, BigInteger>,
+    ) : Fee(feeAssetId, priority, amount, options)
 
     class Regular(
-        override val feeAssetId: AssetId,
-        override val priority: FeePriority,
-        override val amount: BigInteger,
+        feeAssetId: AssetId,
+        priority: FeePriority,
+        amount: BigInteger,
         val maxGasPrice: BigInteger,
         val limit: BigInteger,
-        override val options: Map<String, BigInteger>,
-    ) : Fee
+        options: Map<String, BigInteger>,
+    ) : Fee(feeAssetId, priority, amount, options)
 
     class Eip1559(
-        override val feeAssetId: AssetId,
-        override val priority: FeePriority,
-        override val amount: BigInteger,
+        feeAssetId: AssetId,
+        priority: FeePriority,
+        amount: BigInteger,
         val maxGasPrice: BigInteger,
         val minerFee: BigInteger,
         val limit: BigInteger,
-        override val options: Map<String, BigInteger>,
-    ) : Fee
+        options: Map<String, BigInteger>,
+    ) : Fee(feeAssetId, priority, amount, options)
 
     class Solana(
-        override val feeAssetId: AssetId,
-        override val priority: FeePriority,
-        override val amount: BigInteger,
+        feeAssetId: AssetId,
+        priority: FeePriority,
+        amount: BigInteger,
         val minerFee: BigInteger,
         val maxGasPrice: BigInteger,
         val unitFee: BigInteger,
         val limit: BigInteger,
-        override val options: Map<String, BigInteger>,
-    ) : Fee
+        options: Map<String, BigInteger>,
+    ) : Fee(feeAssetId, priority, amount, options)
 }
