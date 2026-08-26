3978869be29e1e717b9e86164c647a9b72d1301c
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-08-26 18:57:36 +0300

    Start scanning when the scanner appears
    The scan started from makeCoordinator during the view update, writing the isScannerReady binding mid update; a host controller now starts it from viewDidAppear.

--- ios/Features/QRScanner/Sources/Views/QRScannerViewWrapper.swift
+++ ios/Features/QRScanner/Sources/Views/QRScannerViewWrapper.swift
@@ -4,6 +4,37 @@ import SwiftUI
 import VisionKit
 
 @MainActor
+final class ScannerHostViewController: UIViewController {
+    private let scanner: DataScannerViewController
+
+    var onDidAppear: (() -> Void)?
+
+    init(scanner: DataScannerViewController) {
+        self.scanner = scanner
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(scanner)
+        scanner.view.frame = view.bounds
+        scanner.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(scanner.view)
+        scanner.didMove(toParent: self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        onDidAppear?()
+    }
+}
+
+@MainActor
 struct QRScannerViewWrapper {
     typealias ScanResult = (Result<String, Error>) -> Void
 
@@ -49,12 +80,14 @@ struct QRScannerViewWrapper {
 // MARK: - UIViewControllerRepresentable
 
 extension QRScannerViewWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> DataScannerViewController {
+    func makeUIViewController(context: Context) -> ScannerHostViewController {
         dataScannerVC.delegate = context.coordinator
-        return dataScannerVC
+        let host = ScannerHostViewController(scanner: dataScannerVC)
+        host.onDidAppear = startScanning
+        return host
     }
 
-    func updateUIViewController(_: DataScannerViewController, context _: Context) {}
+    func updateUIViewController(_: ScannerHostViewController, context _: Context) {}
 
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -70,7 +103,6 @@ extension QRScannerViewWrapper {
 
         init(_ parent: QRScannerViewWrapper) {
             self.parent = parent
-            parent.startScanning()
         }
 
         func didAddItem(item: RecognizedItem) {
